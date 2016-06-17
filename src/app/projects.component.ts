15d4a6b44491cc483ae576d451d28453dc77ad07
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-06-16 23:21:52 -0500

    refactor: switch from ul to table display for projects component

--- src/app/projects.component.ts
+++ src/app/projects.component.ts
@@ -4,7 +4,16 @@ import {ProjectService} from './project.service';
 
 @Component({
 	selector: 'projects',
-	template: '<h2>Projects</h2><ul><li *ngFor="#project of projects">{{project.title}}</li></ul>',
+	template: '<h2>Projects</h2 >\
+	<table class="table table-hover">\
+	<thead><tr>\
+	<td>image </td><td> description </td><td> title </td></tr>\
+	</thead>\
+	<tbody ><tr *ngFor="#project of projects" >\
+	<td></td>\
+	<td> {{project.description }} </td>\
+	<td> {{project.title }} </td></tr></tbody>\
+	</table>',
 	providers: [ProjectService]
 })
 export class ProjectsComponent {
