a868fd261614ea1d429907ab49ab1d14e3c00b09
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-06-30 09:24:39 -0500

    refactor: skills directory

---
+++ src/app/skills/skills.routes.ts
@@ -0,0 +1,6 @@
+import { RouterConfig } from '@angular/router';
+import { SkillsComponent } from './skills.component';
+
+export const SkillsRoutes : RouterConfig = [
+  { path: 'skills',  component: SkillsComponent }
+];
