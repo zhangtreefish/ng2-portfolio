10571d0cdd3c1313592e294560efcef87396c66f
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-07-20 18:51:50 -0500

    fix:/style: remove id from Projects view; set skills and resume page as redirect page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,17 @@ import { provideRouter, RouterConfig } from '@angular/router';
 
 import { ProjectsRoutes } from './projects/projects.routes';
 import { SkillsRoutes } from './skills/skills.routes';
+// import { SkillsComponent } from './skills/skills.component';
+// import { ProjectsComponent } from './projects/projects.component';
+// import { ProjectComponent } from './projects/project.component';
+
 const routes: RouterConfig = [
-  { path: '', redirectTo: 'projects', terminal: true },
   ...ProjectsRoutes,
-  ...SkillsRoutes
+  ...SkillsRoutes,
+  // { path: 'skills',  component: SkillsComponent },
+  // { path: 'projects',  component: ProjectsComponent },
+  // { path: 'project/:id', component: ProjectComponent },
+  { path: '', redirectTo: 'skills', terminal: true }
 ];
 
 export const APP_ROUTER_PROVIDERS = [
--- src/app/projects/projects.routes.ts
+++ src/app/projects/projects.routes.ts
@@ -4,6 +4,7 @@ import { ProjectsComponent } from './projects.component';
 import { ProjectComponent } from './project.component';
 
 export const ProjectsRoutes : RouterConfig = [
+  // { path: '', redirectTo: 'projects', terminal: true },
   { path: 'projects',  component: ProjectsComponent },
   { path: 'project/:id', component: ProjectComponent }
 ];
